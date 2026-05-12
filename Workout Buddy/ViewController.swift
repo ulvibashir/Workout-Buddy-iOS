34d5c00309f0195b25c0ef603e498084a582faeb
BasirovUS <ulvi.basirov@kapitalbank.az>
2026-05-12 13:58:14 +0400

    Initial Commit

---
+++ Workout Buddy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Workout Buddy
+//
+//  Created by Ulvi Bashirov on 12.05.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
